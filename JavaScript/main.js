4e317f530d4a63be84a6d010ced7ed99689a7497
juan_quiroz82202 <juan_quiroz82202@elpoli.edu.co>
2023-06-08 17:16:00 -0500

    style(add style to the login buttom)

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -274,3 +274,4 @@ function productInfo(e) {
 }
 
 
+
---
+++ product page/script.js
@@ -0,0 +1,5 @@
+var boton = document.getElementById("login");
+
+boton.addEventListener("click", function() {
+  window.location.href = "../Login/index.html";
+});
